baa722918775a83774ec72190141341aa869d05c
Ubuntu <ubuntu@ip-172-31-15-157.ap-northeast-2.compute.internal>
2021-07-11 04:26:17 +0000

    del

--- jaa.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = "H7Ig2CEVtccE2xr7YvfxWxMFeOgDkGfOtMxGT0qY"          # 본인 값으로 변경
-secret = "gu8s8rcs1xLxNOte5H1KDEll1GYHyxHu3jRLdhvd"
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
